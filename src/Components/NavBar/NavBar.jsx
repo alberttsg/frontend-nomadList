324c5f16e8add3b0c000b616ca94f2a0ee4ee6dd
Alberto <albertsg.86@gmail.com>
2023-03-10 15:56:32 +0100

    s

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -8,11 +8,11 @@ export const NavBar = () => {
 
   return (
     <div className='navbar'>
-      <div className='logo' onClick={()=>navigate('/')}><b><span>nomad</span></b></div>
+      <div className='logo' onClick={()=>navigate('/')}>nomad</div>
       <div className='container-nav'>
         <div className='home' onClick={()=>navigate('/')}><HomeFilled className='iconHome'/><div className='divHome'>Home</div> </div>
         <div className='profile' onClick={()=>navigate('/profile')}><UserOutlined className='iconProfile'/><div className='divProfile'>Profile</div></div>
-        <div className='create'><PlusCircleFilled className='iconCreate'/><div className='divCreate'>Crear post</div></div>
+        <div className='create'><PlusCircleFilled className='iconCreate'/><div className='divCreate'>Crear</div></div>
       </div>
     </div>
   )
