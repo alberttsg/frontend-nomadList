ac26be5854f0391d5ee97271f29ba3f267acd62d
Alberto <albertsg.86@gmail.com>
2023-03-14 18:04:37 +0100

    w

--- src/Components/CreatePost/PostCreated.jsx
+++ src/Components/CreatePost/PostCreated.jsx
@@ -4,7 +4,7 @@ import './PostCreated.scss'
 export const PostCreated = () => {
   return (
     <div className='post-created'>
-      <div><b>P<span>ost</span> cr<span>e</span>a</b>do</div>
+      <div>Post creado</div>
     </div>
   )
 }
