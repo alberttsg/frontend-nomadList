620f946bc7a8992f4bdd5aadecd106ca4ff874ca
edmondhillary <edu14937@gmail.com>
2023-03-19 17:16:32 +0100

    on my way to do profile id

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -2,14 +2,19 @@
 import './Content.scss'
 import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/UsersState";
+import UserCard from '../UserCard/UserCard';
+import EditUser from '../EditUser/EditUser';
+
 export const Content = () => {
   const { getUserInfo, user, deleteUser, logOut, reset } = useContext(GlobalContext);
   useEffect(()=>{
     getUserInfo()
+
   },[])
   return (
     <div className='content'>
-      hola que tal
+<UserCard/>
+ 
     </div>
   )
 }
--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import { DeleteTwoTone, EditOutlined } from '@ant-design/icons';
+import { Avatar, Card, Modal } from 'antd';
+import React, { useState } from 'react';
+import { GlobalContext } from "../../context/UsersState";
+import EditUser from "../../Components/EditUser/EditUser";
+import { useNavigate } from 'react-router';
+import './UserCard.scss';
+
+
 
 const UserCard = () => {
+
     return (
-        <div>
-            
-        </div>
+ 
+      <div className='card-info-container'>
+         <div className='left-avatar'><Avatar size={138}
+            src={'https://img.freepik.com/vector-premium/lindo-retrato-hombres-pelo-largo-rubio-avatar-aislado-fondo-blanco_555467-2696.jpg?w=2000'}/>
+            </div>
+           <div className='right-info'>
+
+            <div className='first-line'>
+              <span>nombre</span>
+              <button>siguiendo</button>
+              <button>enviar mensaje</button>
+            </div>
+            <div className='second-line'>
+              <span>publicaciones</span>
+              <span>Seguidores</span>
+              <span>seguidos</span>
+
+            </div>
+            <div className='third-line'>
+              <span >Bio: </span>
+              <span>oficio, beneificio edad</span>
+            </div>
+
+           </div>
+
+       </div>
+       
+      
     );
 };
 
