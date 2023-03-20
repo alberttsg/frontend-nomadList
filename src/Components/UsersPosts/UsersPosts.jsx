5be0cc75b630f0bcb183417e7c5f062b2ea7a1e4
edmondhillary <edu14937@gmail.com>
2023-03-20 21:26:19 +0100

    not finished

--- src/Components/UsersPosts/UsersPosts.jsx
+++ src/Components/UsersPosts/UsersPosts.jsx
@@ -3,9 +3,9 @@ import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/UsersState";
 import axios from 'axios';
 import './UserPosts.scss'
-import { createLogger } from 'vite';
 
-const UserPosts =  () => {
+
+const UsersPosts =  () => {
     const { editUser, user, getUserInfo, deleteUser} = useContext(GlobalContext);
     const [posts, setPosts] = useState([]);
 
@@ -22,31 +22,29 @@ const UserPosts =  () => {
         const res = await axios.get(`https://backend-nomadsociety-development.up.railway.app/post/userPosts/${id}`, config);
         setPosts(res.data)
         console.log(res.data)
-    }
-    getPosts(user._id)
-
-    console.log(posts)
-  }, [])
-
-  
-    
-
+      }
+      getPosts(user._id);
+      
+      
+      
+    }, [])
     return (
-
             <div className='posts-container-profiles'>
-                {/* {console.log(posts)}
-                {posts && posts.length > 0 && posts.map((post) => (
+                {console.log(posts)}
+                {posts && posts.map((post) => (
                     <div key={post._id} className='post-container-uni'>
                         <h3>{post.title}</h3>
                         <p>{post.content}</p>
-                        <img src={ post.imagePost || 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'} alt="img" />
+                        {/* <img src={'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'} alt="img" /> */}
                         <button onClick={() => console.log('borra')}>Delete</button>
                         <button onClick={() => console.log('edit')}>Edit</button>
                     </div>
-                ))} */}
+                )
+                )
+                }
             </div>
             
                 );
 };
 
-export default UserPosts;
+export default UsersPosts;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: { exclude: ["fsevents"] },
+
   plugins: [react()],
 })
