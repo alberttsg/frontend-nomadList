6ad04bc40ce2a03959ff18b40e452b9ee3c5ebf5
edmondhillary <edu14937@gmail.com>
2023-03-20 22:00:42 +0100

    hello, almost there

--- src/Components/UsersPosts/UsersPosts.jsx
+++ src/Components/UsersPosts/UsersPosts.jsx
@@ -3,6 +3,8 @@ import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/UsersState";
 import axios from 'axios';
 import './UserPosts.scss'
+import { Card } from 'antd';
+import Meta from 'antd/es/card/Meta';
 
 
 const UsersPosts =  () => {
@@ -21,7 +23,7 @@ const UsersPosts =  () => {
     const getPosts = async (id) => {
         const res = await axios.get(`https://backend-nomadsociety-development.up.railway.app/post/userPosts/${id}`, config);
         setPosts(res.data)
-        console.log(res.data)
+        console.log(posts.length)
       }
       getPosts(user._id);
       
@@ -32,13 +34,18 @@ const UsersPosts =  () => {
             <div className='posts-container-profiles'>
                 {console.log(posts)}
                 {posts && posts.map((post) => (
-                    <div key={post._id} className='post-container-uni'>
-                        <h3>{post.title}</h3>
-                        <p>{post.content}</p>
+                    <Card key={post._id} className='post-container-uni' hoverable
+                    style={{ width: 250 }} cover={<img alt="example" src={ "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png"} />}>
+                        <Meta title={post.title} description={post.content}/>
+                        {/* <p>{post.content}</p> */}
                         {/* <img src={'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'} alt="img" /> */}
+                        <br />
+                        <div className='button-container-posts'>
+
                         <button onClick={() => console.log('borra')}>Delete</button>
                         <button onClick={() => console.log('edit')}>Edit</button>
-                    </div>
+                        </div>
+                    </Card>
                 )
                 )
                 }
