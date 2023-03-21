a9372a02e7715f744c4bee7c4c3e8f598018e6b3
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-21 19:25:45 +0100

    update

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/UsersState";
 import { LikeButton } from '../LikeButton/LikeButton';
 import { PostComponent } from '../PostComponent/PostComponent';
+import { ChatLayout } from '../Chat/components/ChatLayout';
 export const Content = () => {
   const { getUserInfo, user, deleteUser, logOut, reset } = useContext(GlobalContext);
   useEffect(()=>{
@@ -12,6 +13,7 @@ export const Content = () => {
   return (
     <div className='content'>
       <PostComponent />
+      <ChatLayout/>
     </div>
   )
 }
