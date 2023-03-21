d6de07b5e2120b2b1df925467f45a2935a2cfae7
danicaroo940 <danicaroo94@gmail.com>
2023-03-21 02:09:04 +0100

    Modificamos fecha de creacion de los post con traducción

--- src/Components/DateComponent/DateComponent.jsx
+++ src/Components/DateComponent/DateComponent.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime';
+import * as locale from 'dayjs/locale/es'
 
 
 export const DateComponent = (props) => {
+  dayjs.locale(locale)
   dayjs.extend(relativeTime)
   const {datePost} = props
   const dateString = dayjs(datePost).format('YYYY-MM-DD')
+  const time = dayjs().to(dayjs(dateString))
   return (
     <div>
-      {dayjs().to(dayjs(dateString))}
+      <span>{time} <br></br>{datePost}</span>
     </div>
   )
 }
