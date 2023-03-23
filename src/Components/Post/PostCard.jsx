a1d7aa1693f62fef120d440001daa270aac8e659
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-23 15:45:56 +0100

    update

--- src/Components/Post/PostCard.jsx
+++ src/Components/Post/PostCard.jsx
@@ -9,21 +9,21 @@ export function PostCard(props) {
   return (
     <Card hoverable className="post-content">
       <div ref={forwardedRef} style={{ width: '100%' }}>
-        <h3>{post.title}</h3>
-        {post.author.displayName}
+        <h3>{post?.title}</h3>
+        {post?.author?.displayName}
         <Carousel>
-          {post.imagePost.map(img =>
+          {post?.imagePost?.map(img =>
             <img className='post-img' key={img} src={img} alt="Resource not found" />
           )}
         </Carousel>
         <div style={{ border: '1px solid lightgrey', borderRadius: '5px', padding: '5px' }}>
-          {post.content}
+          {post?.content}
         </div>
       </div>
       <div style={{ width: '100%' }}>
-        <LikeButton id={post._id} likes={post.likesCount} />
-        <CommentsPrint postId={post._id} />
-        <DateComponent datePost={post.createdAt} />
+        <LikeButton id={post?._id} likes={post?.likesCount} />
+        <CommentsPrint postId={post?._id} />
+        <DateComponent datePost={post?.createdAt} />
       </div>
     </Card>
   )
