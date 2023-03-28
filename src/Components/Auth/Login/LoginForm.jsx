308a74cdc9b3bb740ed6a815e433862dfd4f7edc
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-29 01:08:01 +0200

    fixed login button align

--- src/Components/Auth/Login/LoginForm.jsx
+++ src/Components/Auth/Login/LoginForm.jsx
@@ -61,11 +61,12 @@ const LoginForm = () => {
       >
         <Input.Password />
       </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+
+      <div style={{ display: 'flex', flexFlow: 'row nowrap', justifyContent: 'center' }}>
         <Button type="primary" htmlType="submit">
           Login
         </Button>
-      </Form.Item>
+      </div>
     </Form>
   )
 }
