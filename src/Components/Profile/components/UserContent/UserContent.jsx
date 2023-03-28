a75be8ad949508ce486dc30da82a406e6e0fe8b4
edmondhillary <edu14937@gmail.com>
2023-03-28 21:18:23 +0200

    juju

--- src/Components/Profile/components/UserContent/UserContent.jsx
+++ src/Components/Profile/components/UserContent/UserContent.jsx
@@ -6,22 +6,23 @@ export function UserContent() {
   return (
     <Row style={{ width: '100%' }} justify='center' align='middle'>
       <Tabs
-        style={{ width: '100%' }}
+        style={{ width: '90%' }}
         tabBarGutter={10}
         items={[
           {
             key: '1',
-            label: <h3><ThunderboltFilled style={{ color: "#F0C311" }} />POSTS</h3>,
+            label:<div><h3><ThunderboltFilled style={{ color: "#F0C311" }} />POSTS</h3></div>,
             children: <UserPosts />,
             forceRender: true,
           },
           {
             key: '2',
-            label: <h3><CommentOutlined />COMMENTS</h3>,
+            label: <div style={{ padding: ' 0 10px ' }}><h3 ><CommentOutlined style={{ color: "rgb(152,2,154,0.8)"}}  />COMMENTS</h3></div>,
             forceRender: true,
           },
         ]}
       />
+
     </Row>
   )
 }
