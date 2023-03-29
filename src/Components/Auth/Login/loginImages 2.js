679ce9a3b6c62f0a25b5b816ce221eac3e1f16d6
edmondhillary <edu14937@gmail.com>
2023-03-30 00:44:18 +0200

    juju

--- src/Components/Auth/Login/loginImages 2.js
+++
@@ -1,5 +0,0 @@
-export const images = [
-    'https://res.cloudinary.com/lauradohle/image/upload/v1678500630/nomad-socialNetwork/pexels-ma%C3%ABl-balland-2076968_dmncwo.jpg',
-    'https://res.cloudinary.com/lauradohle/image/upload/v1678500630/nomad-socialNetwork/pexels-kyle-roxas-2187629_kob09h.jpg',
-    'https://res.cloudinary.com/lauradohle/image/upload/v1678500629/nomad-socialNetwork/pexels-shaan-johari-2405041_dv31xq.jpg',
-  ]
