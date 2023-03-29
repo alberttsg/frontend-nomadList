a0e7a16ffa2d4e2086e3b21b9f97e1c665c3b21b
Christian Meneses <christian.meneses.ls@gmail.com>
2023-03-30 01:17:45 +0200

    Logo button in resources

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import React, { useContext, useState } from 'react'
 import { Avatar, Modal } from 'antd';
 import { GlobalContext } from '../../context/UsersState'
-import { CreatePostNew } from '../CreatePostNew/CreatePostNew';
+import { CreatePostNew } from '../CreatePostNew/CreatePostNew'
+import { Logo } from '../../resources/Logo'
 import './NavBar.scss'
 
 export const NavBar = () => {
@@ -18,19 +19,19 @@ export const NavBar = () => {
   };
   const handleCancel = () => {
     setIsModalOpen(false);
-    
+
   };
 
   return (
     <div className='navbar'>
-      <div className='logo' onClick={() => navigate('/')}>nomad</div>
+      <Logo/>
       <div className='container-nav'>
         <div className='home' onClick={() => navigate('/')}><HomeFilled className='iconHome' /><div className='divHome'>Home</div> </div>
         <div className='profile' onClick={() => navigate('/profile')}><Avatar style={{
           objectFit: 'cover',
           border: '0.5px solid rgb(0,0,0, 0.7)'
-        }} size={24} shape={'circle'} src={user?.avatar}/><div className='divProfile'style={{
-          padding:'0'
+        }} size={24} shape={'circle'} src={user?.avatar} /><div className='divProfile' style={{
+          padding: '0'
         }}>Profile</div></div>
         <div className='create' onClick={() => setIsModalOpen(!isModalOpen)} ><PlusCircleFilled className='iconCreate' /><div className='divCreate'>Post</div></div>
         <div className='logout' onClick={() => navigate('/countries')}>
@@ -39,13 +40,13 @@ export const NavBar = () => {
         <div className='logout'
           onClick={() => {
             logOut(),
-            navigate('/')
+              navigate('/')
             reset()
           }}>
           <UnlockFilled className='iconlogout' /><div className='divLogout'>Log out</div></div>
 
       </div>
-      <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} centered  footer={null} >
+      <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} centered footer={null} >
         <CreatePostNew onAction={handleCancel} />
       </Modal>
     </div>
