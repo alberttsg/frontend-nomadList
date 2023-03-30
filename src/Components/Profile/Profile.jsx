46e9548193f5d93c152786fe62323b01c889de68
edmondhillary <edu14937@gmail.com>
2023-03-30 16:38:37 +0200

    hellou

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import { UserCard } from "./components/UserCard/UserCard";
 import { UserContent } from "./components/UserContent/UserContent";
 import { getUserById } from "../../service/userService";
 import CountryUser from "./components/CountryUser/CountryUser";
-import { Spin, Row, Divider } from "antd";
+import { Spin, Row, Divider, Skeleton } from "antd";
 
 export const ProfileContext = createContext();
 
@@ -30,7 +30,16 @@ export const Profile = () => {
 
   return (
     <ProfileContext.Provider value={{ userData, setUserData }}>
-      <Spin spinning={isLoading}>
+      <Skeleton style={{
+        padding: '50px',
+        display: "flex",
+        justifyContent: "center",
+        alignContent: "center",
+        width: '100%'
+      }}  paragraph={{
+        rows: 20,
+     
+      }} size={'small'} avatar loading={isLoading}>
        
         <Row>
 
@@ -45,7 +54,7 @@ export const Profile = () => {
         <Row>
           <UserContent />
         </Row>
-      </Spin>
+      </Skeleton>
     </ProfileContext.Provider>
   );
 };
